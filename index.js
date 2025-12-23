696825fcf2846b486872d09302c950e7e6d9f84c
cracker228 <trunotlal66@gmail.com>
2025-12-23 13:23:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const bot = new Telegraf(BOT_TOKEN);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 // ===== FILES =====
 const DATA_DIR = path.join(__dirname, 'catalogs');
