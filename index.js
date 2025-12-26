0be09db290e09ec77a6331c38f31b5963cac928c
cracker228 <trunotlal66@gmail.com>
2025-12-26 14:49:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -209,6 +209,19 @@ bot.on('photo', ctx => {
     );
   }
 });
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log('🌐 HTTP server on port', PORT);
+});
+
 
 /* ================== LAUNCH ================== */
 bot.launch();
